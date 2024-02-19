b7e26fddd7a39d1c26dae809fc023397ff8deb65
PRIYAM PATEL <patelpriyam79@gmail.com>
2024-02-18 23:41:09 -0500

    Initial Commit

---
+++ To Do List/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  To Do List
+//
+//  Created by PRIYAM PATEL on 18/02/24.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
