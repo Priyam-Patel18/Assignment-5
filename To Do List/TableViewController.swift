f4b3fa92496851fd22f3c736c199d3ba748b76f1
PRIYAM PATEL <patelpriyam79@gmail.com>
2024-02-19 14:18:30 -0500

    Final Commit

--- To Do List/TableViewController.swift
+++ To Do List/TableViewController.swift
@@ -7,58 +7,60 @@
 
 import UIKit
 
+//-----------------------------------Taking a structure for the list items----------------------------------------------------
+
 struct TodoItem: Codable {
     var title: String
 }
 
+//--------------------------------------------Main Program--------------------------------------------------------------------
 class TableViewController: UITableViewController {
 
-    var todoItems = [TodoItem]()
+    var taskLists = [TodoItem]()
        
        override func viewDidLoad() {
            super.viewDidLoad()
            loadTodoItems()
        }
        
-       // MARK: - Table view data source
+//----------------------------------------Input Table view data source--------------------------------------------------------
        
        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return todoItems.count
+           return taskLists.count
        }
        
        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
            let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
-           let todoItem = todoItems[indexPath.row]
+           let todoItem = taskLists[indexPath.row]
            cell.textLabel?.text = todoItem.title
            return cell
        }
        
-       // MARK: - Actions
+    
+//-------------------------------------------------Making Add Button Action---------------------------------------------------
+       // Add Button Action to call the Method
    
    
     @IBOutlet weak var addButton: UIButton!
     
     @IBAction func addButton(_ sender: UIButton) {
-        showAddTodoAlert()
+        AddTodoAlert()
     }
     
-    /*@IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
-        showAddTodoAlert()
-       }*/
+//----------------------------------------Function to add show alert and take user input--------------------------------------
        
-       // MARK: - Private Methods
-       
-       private func showAddTodoAlert() {
-           let alertController = UIAlertController(title: "Add Todo", message: nil, preferredStyle: .alert)
+    //Method to add tasks into To-Do Lists
+       private func AddTodoAlert() {
+           let alertController = UIAlertController(title: "Enter Task", message: nil, preferredStyle: .alert)
            
            alertController.addTextField { textField in
-               textField.placeholder = "Enter todo item"
+               textField.placeholder = "Task Details"
            }
            
            let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
                if let textField = alertController.textFields?.first, let text = textField.text, !text.isEmpty {
                    let newTodoItem = TodoItem(title: text)
-                   self?.todoItems.append(newTodoItem)
+                   self?.taskLists.append(newTodoItem)
                    self?.saveTodoItems()
                    self?.tableView.reloadData()
                }
@@ -71,29 +73,30 @@ class TableViewController: UITableViewController {
            
            present(alertController, animated: true, completion: nil)
        }
-       
-        private func saveTodoItems() {
-           // Implement data persistence to save todoItems
-           // For example, you can use UserDefaults or Core Data
-           // UserDefaults is used in this example for simplicity
-           let data = try? JSONEncoder().encode(todoItems)
+ 
+//------------------------------------Functions to Save and Retrive the Data--------------------------------------------------
+    
+    //Using JSONEncoder to save all the task lists
+    private func saveTodoItems() {
+           let data = try? JSONEncoder().encode(taskLists)
            UserDefaults.standard.set(data, forKey: "todoItems")
        }
         
        
+    //Calling saved task lists when reopening the app (load items into the cell)_
        private func loadTodoItems() {
-           // Load todoItems from data persistence
            if let data = UserDefaults.standard.data(forKey: "todoItems"),
               let savedTodoItems = try? JSONDecoder().decode([TodoItem].self, from: data) {
-               todoItems = savedTodoItems
+               taskLists = savedTodoItems
            }
        }
        
-       // MARK: - Table view delegate
-       
+//---------------------------------------Input Table View Delegate------------------------------------------------------------
+    
+       //Table View Delegate
        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
            if editingStyle == .delete {
-               todoItems.remove(at: indexPath.row)
+               taskLists.remove(at: indexPath.row)
                saveTodoItems()
                tableView.deleteRows(at: [indexPath], with: .fade)
            }
