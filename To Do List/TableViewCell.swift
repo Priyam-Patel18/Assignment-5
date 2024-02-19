52e8223a834d7bac1867b5f4e7716bec9885157c
PRIYAM PATEL <patelpriyam79@gmail.com>
2024-02-18 23:41:50 -0500

    Second Commit (Updated Code)

--- To Do List/TableViewCell.swift
+++ To Do List/TableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    @IBOutlet weak var label: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- To Do List/TableViewController.swift
+++ To Do List/TableViewController.swift
@@ -7,83 +7,95 @@
 
 import UIKit
 
+struct TodoItem: Codable {
+    var title: String
+}
+
 class TableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    var todoItems = [TodoItem]()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           loadTodoItems()
+       }
+       
+       // MARK: - Table view data source
+       
+       override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return todoItems.count
+       }
+       
+       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+           let todoItem = todoItems[indexPath.row]
+           cell.textLabel?.text = todoItem.title
+           return cell
+       }
+       
+       // MARK: - Actions
+   
+   
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        showAddTodoAlert()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    /*@IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        showAddTodoAlert()
+       }*/
+       
+       // MARK: - Private Methods
+       
+       private func showAddTodoAlert() {
+           let alertController = UIAlertController(title: "Add Todo", message: nil, preferredStyle: .alert)
+           
+           alertController.addTextField { textField in
+               textField.placeholder = "Enter todo item"
+           }
+           
+           let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
+               if let textField = alertController.textFields?.first, let text = textField.text, !text.isEmpty {
+                   let newTodoItem = TodoItem(title: text)
+                   self?.todoItems.append(newTodoItem)
+                   self?.saveTodoItems()
+                   self?.tableView.reloadData()
+               }
+           }
+           
+           let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+           
+           alertController.addAction(addAction)
+           alertController.addAction(cancelAction)
+           
+           present(alertController, animated: true, completion: nil)
+       }
+       
+        private func saveTodoItems() {
+           // Implement data persistence to save todoItems
+           // For example, you can use UserDefaults or Core Data
+           // UserDefaults is used in this example for simplicity
+           let data = try? JSONEncoder().encode(todoItems)
+           UserDefaults.standard.set(data, forKey: "todoItems")
+       }
+        
+       
+       private func loadTodoItems() {
+           // Load todoItems from data persistence
+           if let data = UserDefaults.standard.data(forKey: "todoItems"),
+              let savedTodoItems = try? JSONDecoder().decode([TodoItem].self, from: data) {
+               todoItems = savedTodoItems
+           }
+       }
+       
+       // MARK: - Table view delegate
+       
+       override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+           if editingStyle == .delete {
+               todoItems.remove(at: indexPath.row)
+               saveTodoItems()
+               tableView.deleteRows(at: [indexPath], with: .fade)
+           }
+       }
 }
